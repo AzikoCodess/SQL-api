934d21b1480aee8a09652641d28181c781326755
AzikoCodess <cybersport0109@gmail.com>
2026-03-24 22:57:55 +0500

    sql organilyabdi

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express()
 app.use(express.json())
 
 const db = new sqlite3.Database("./db.db", (err) => {
-    if (err){
-        console.log(err.message)
-    } else{
-        console.log("DB ulandi")
-    }
+  if (err) {
+    console.log(err.message)
+  } else {
+    console.log("DB ulandi")
+  }
 })
 
 
@@ -59,7 +59,49 @@ app.post("/users", (req, res) => {
 
 })
 
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params
+  const sql = "SELECT * FROM users WHERE id = ?"
+  db.get(sql, [id], (err, row) => {
+    if (err) {
+      return res.json(err.message)
+    }
+    res.json(row)
+  })
+})
+
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params
+  const { name, age } = req.body
+
+  const sql = "UPDATE users SET name=?, age=? WHERE id=?"
+
+  db.run(sql, [name, age, id], function (err) {
+    if (err) {
+      return res.json(err.message)
+    }
+    const user = {
+      name, age, id
+    }
+    res.json({ message: "updated", user })
+  })
+})
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params
+
+  const sql = "DELETE FROM users WHERE id=?"
+
+  db.run(sql, [id], function (err) {
+    if (err) {
+      return res.json(err.message)
+    }
+    res.json("deleted")
+  })
+})
+
+
 
 app.listen(3000, () => {
-    console.log("Server ishladi")
+  console.log("Server ishladi")
 })

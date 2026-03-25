5f119f292ad93dfdbde7f8b376fadfae1314647c
AzikoCodess <cybersport0109@gmail.com>
2026-03-25 18:03:31 +0500

    auth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const sqlite3 = require("sqlite3").verbose()
-
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const app = express()
 app.use(express.json())
 
@@ -19,7 +20,7 @@ db.run(`
 CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    age INTEGER
+    password TEXT
 )
 `)
 
@@ -97,6 +98,175 @@ app.delete("/delete/:id", (req, res) => {
       return res.json(err.message)
     }
     res.json("deleted")
+  })
+})
+
+app.post("/orders", (req, res) => {
+  const { product, price, user_id } = req.body
+
+  const sql = "INSERT INTO orders (product, price, user_id) VALUES (?,?,?)"
+
+  db.run(sql, [product, price, user_id], function (err) {
+    if (err) {
+      return res.json(err.message)
+    }
+    res.json({
+      id: this.lastID
+    })
+  })
+})
+
+app.get("/orders", (req, res) => {
+  const sql = `
+  SELECT
+    orders.id,
+    orders.product,
+    orders.price,
+    users.name
+  FROM orders
+  INNER JOIN users
+  ON orders.user_id = users.id
+  `
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return res.json(err.message)
+    }
+    res.json(rows)
+  })
+})
+
+app.get("/ordersLeft", (req, res) => {
+  const sql = `
+  SELECT
+    users.name,
+    orders.product
+  FROM users
+  LEFT JOIN orders
+  ON users.id = orders.user_id
+  `
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return res.json(err.message)
+    }
+    res.json(rows)
+  })
+})
+//shunchaki ishlatib kordim lekn buni alohida yoz
+app.get("/search", (req, res) => {
+  const txt = req.query.txt
+  const limit = req.query.limit
+  console.log(txt)
+  console.log(limit)
+  if (txt) {
+    const sql = `SELECT * FROM users WHERE name LIKE ?`
+    db.all(sql, [`%${txt}%`], (err, rows) => {
+      if (err) {
+        return res.json(err.message)
+      }
+      res.json(rows)
+    })
+  } else if (limit) {
+    const sqlLimit = "SELECT * FROM users LIMIT ?"
+    db.all(sqlLimit, [limit], (err, rows) => {
+      if (err) {
+        return res.json(err.message)
+      }
+      res.json(rows)
+    })
+  }
+
+})
+
+app.get("/users", (req, res) => {
+  const page = Number(req.query.page) || 1
+  const limit = 2
+  const offset = (page - 1) * limit
+  const sql = "SELECT * FROM users LIMIT ? OFFSET ?"
+
+  db.all(sql, [limit, offset], (err, rows) => {
+    res.json(rows)
+  })
+
+})
+
+// const password = "546132"
+// bcrypt.hash(password, 10, (err, hash) => {
+//   console.log(hash)
+//   console.log(err)
+// })
+
+app.post("/register", (req, res) => {
+  const { name, password } = req.body
+  bcrypt.hash(password, 10, (err, hash) => {
+    const sql = "INSERT INTO users (name, password) VALUES (?,?)"
+    db.run(sql, [name, hash], function (err) {
+      if (err) {
+        return res.json(err.message)
+      }
+      res.json("Created")
+    })
+  })
+})
+
+// bcrypt.compare("546132", "$2b$10$Y9pOSEH8A2lyy5Mv49Bwg.J8NyhBWIY.1N.1OeYIIPufPqiqLG4wO", (err, result) => {
+//   console.log(result)
+// })
+
+app.post("/login", (req, res) => {
+  const { name, password } = req.body
+
+  const sql = "SElECT * FROM users WHERE name=?"
+  db.get(sql, [name], (err, user) => {
+    if(err){
+      return res.json(err.message)
+    }
+    if (!user) {
+      return res.json("user yo'q")
+    }
+    bcrypt.compare(password, user.password, (err, ok) => {
+      if(err){
+        return res.json(err.message)
+      }
+      if (!ok) {
+        return res.json("wrong password")
+      }
+      const token = jwt.sign(
+        { id: user.id }, "secretKey"
+      )
+      res.json(token)
+    })
+  })
+})
+
+// const token = jwt.sign(
+//   {id: 1},
+//   "secret123"
+// )
+// const decoded = jwt.verify(token, "secret123")
+// console.log(decoded)
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if(!authHeader){
+    return res.json("token yoq")
+  }
+  const token = authHeader && authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : authHeader
+  try {
+    const decoded = jwt.verify(token, "secretKey")
+    req.user = decoded
+    next()
+  } catch (error) {
+    console.log(error)
+    return res.json("token xato")
+  }
+  
+}
+app.get("/profile", authMiddleware, (req, res) => {
+  res.json({
+    message: "protected route",
+    user: req.user.id
   })
 })
 
